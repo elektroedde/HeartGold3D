381c3c492ab61377352b670bfb64afa4a591f653
elektroedde <edvinberling@gmail.com>
2024-03-28 22:23:06 +0100

    Initial Commit

---
+++ HeartGold3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartGold3D
+//
+//  Created by Edvin Berling on 2024-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartGold3D/HeartGold3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartGold3DApp.swift
+//  HeartGold3D
+//
+//  Created by Edvin Berling on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct HeartGold3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
